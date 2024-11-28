ed1031bb759b0387a0c4ba8c1aece60ea2ea8535
Sawatri <sawatribs@gmail.com>
2024-11-28 10:39:00 +0500

    Added reminder operator

--- Arithmetic Operators/ArithmeticOperators.java
+++ Arithmetic Operators/ArithmeticOperators.java
@@ -8,12 +8,13 @@ int minus = sum -num2;
 int multiply = num*num2;
 int divide = num2/num;
 float divide2 = (float)num/num2;
+float rem = num1%num2
 
 System.out.println("sum"+sum);
 System.out.println("minus"+minus);
 System.out.println("multiply"+multiply);
 System.out.println("divide"+divide);
 System.out.println("divide"+divide2);
-
+System.out.println("Reminder"+rem);
 }
 }
