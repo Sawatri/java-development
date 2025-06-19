df3b8052a04f1da92986eecfcb35f87d999f4f75
Sawatri <sawatribs@gmail.com>
2025-06-19 15:49:10 +0500

    changes

--- SQL/department.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM library_management_system.department;
-
-USE library_management_system;
-CREATE TABLE department(
-dept_id INT primary key auto_increment,
-dept_name VARCHAR(50),
-dept_code INT);
